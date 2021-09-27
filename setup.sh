3cc5850e7e4a90c0c3f967f94b6cb9e785021a39
Lindsey Bieda <hazardous.narayan@gmail.com>
2021-09-27 11:34:43 -0400

    Add some debugging

--- setup.sh
+++ setup.sh
@@ -6,4 +6,7 @@ git config --global core.editor "vim"
 git config --global push.default simple
 git config --global commit.gpgsign true
 
+touch ".dotfiles-loaded"
+echo "Dotfiles loaded"
+
 source .bash_profile
