5b3610270396afdf0cee4571cd1cac76929d0676
Lindsey Bieda <hazardous.narayan@gmail.com>
2021-09-28 09:29:02 -0400

    Update and rename setup.sh to install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,10 @@
 
 set -x
 
+exec > >(tee -i $HOME/dotfiles_install.log)
+exec 2>&1
+set -x
+
 git config --global core.editor "vim"
 git config --global push.default simple
 git config --global commit.gpgsign true
